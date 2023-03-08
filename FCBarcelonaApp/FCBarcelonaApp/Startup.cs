a654f8a9433ff8aec7c429877c02a43318e79bf3
martushoka <martushoka.10@gmail.com>
2023-03-08 12:31:52 +0200

    seedadmin

--- FCBarcelonaApp/FCBarcelonaApp/Startup.cs
+++ FCBarcelonaApp/FCBarcelonaApp/Startup.cs
@@ -1,5 +1,6 @@
 using FCBarcelonaApp.Data;
 using FCBarcelonaApp.Domain;
+using FCBarcelonaApp.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -56,6 +57,7 @@ namespace FCBarcelonaApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.PrepareDatabase();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
